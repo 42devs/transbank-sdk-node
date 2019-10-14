8ce61ae6deff119114bfbeecad0b6c40ce996477
Nicolas Martinez <nicolas.martinez@humandaft.cl>
2019-10-14 08:26:42 -0300

    add cors headers

--- utils/request.js
+++ utils/request.js
@@ -5,7 +5,8 @@ module.exports = (opts) => {
     return axios.create({
       baseURL: opts.baseURL,
       headers: {
-        'content-type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json;charset=utf-8',
         'Tbk-Api-Key-Id': opts.commerceCode,
         'Tbk-Api-Key-Secret': opts.apiKey,
       },
@@ -14,6 +15,8 @@ module.exports = (opts) => {
   return axios.create({
     baseURL: opts.baseURL,
     headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'application/json;charset=utf-8',
       Authorization: opts.apiKey,
       commerceCode: opts.commerceCode,
     },
