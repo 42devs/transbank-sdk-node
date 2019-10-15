3eb7e81d26accb6c48752752f68a89f2c4df6876
Nicolas Martinez <nicolas.martinez@humandaft.cl>
2019-10-15 07:55:54 -0300

    wip cors

--- utils/request.js
+++ utils/request.js
@@ -1,24 +1,17 @@
 const axios = require('axios');
 
-module.exports = (opts) => {
-  if (!('commerce' in opts)) {
-    return axios.create({
-      baseURL: opts.baseURL,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Content-Type': 'application/json;charset=utf-8',
-        'Tbk-Api-Key-Id': opts.commerceCode,
-        'Tbk-Api-Key-Secret': opts.apiKey,
-      },
-    });
-  }
-  return axios.create({
-    baseURL: opts.baseURL,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Content-Type': 'application/json;charset=utf-8',
-      Authorization: opts.apiKey,
-      commerceCode: opts.commerceCode,
-    },
-  });
-};
+module.exports = (opts) => (!('commerce' in opts) ? axios.create({
+  baseURL: opts.baseURL,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+    'Tbk-Api-Key-Id': opts.commerceCode,
+    'Tbk-Api-Key-Secret': opts.apiKey,
+  },
+}) : axios.create({
+  baseURL: opts.baseURL,
+  headers: {
+    Authorization: opts.apiKey,
+    commerceCode: opts.commerceCode,
+  },
+}));
