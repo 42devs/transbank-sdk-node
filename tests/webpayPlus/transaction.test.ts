0a27cd42e9ea8ec31e0f07fdcfff8575bfad01d7
Nicolas Martinez V <nmartinezv@icloud.com>
2021-12-14 01:20:57 -0300

    feat: test request transaction

--- tests/webpayPlus/transaction.test.ts
+++ tests/webpayPlus/transaction.test.ts
@@ -4,7 +4,7 @@ import { makeId } from '../helper';
 
 describe('Webpay Plus', () => {
   describe('Normal Transaction', () => {
-    it('create transaction', async () => {
+    it('OK - create transaction', async () => {
       const buyOrder = makeId(6);
       const sessionId = makeId(10);
       const amount = 1000;
@@ -14,10 +14,22 @@ describe('Webpay Plus', () => {
         apiKey: '579B532A7440BB0C9079DED94D31EA1615BACEB56610332264630D42D0A36B1C',
         environment: 'test',
       };
-
       const response = await create(buyOrder, sessionId, amount, returnUrl, options);
-      console.log(response);
       expect(response).toBeDefined();
+      expect(response).toHaveProperty('token');
+    });
+    it('Error - Can not make request', async () => {
+      const buyOrder = makeId(6);
+      const sessionId = makeId(10);
+      const amount = 1000;
+      const returnUrl = 'http://localhost:3000/transaction/return';
+      const options: IOptions = {
+        commerceCode: '59705a555532',
+        apiKey: '579B532A7440BB0C9079DED94D31EA1615BACEB56610332264630D42D0A36B1C',
+        environment: 'test',
+      };
+      const response = await create(buyOrder, sessionId, amount, returnUrl, options);
+      expect(response).toHaveProperty('message');
     });
   });
 });
